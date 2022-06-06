836695e8434d79b6cfa8cd9829d8bc733629d639
Natalia Sereda <natalia.sereda.ex@gmail.com>
2022-06-06 10:59:37 +0300

    create animation in three.js

---
+++ src/App.js
@@ -0,0 +1,98 @@
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import {
+  CubeCamera,
+  Environment,
+  OrbitControls,
+  PerspectiveCamera,
+} from "@react-three/drei";
+import { Ground } from "../src/Ground";
+import { Car } from "../src/Car";
+import { Rings } from "../src/Rings";
+import { Boxes } from "../src/Boxes";
+import "./App.css";
+import {
+  Bloom,
+  ChromaticAberration,
+  DepthOfField,
+  EffectComposer,
+} from "@react-three/postprocessing";
+import { BlendFunction } from "postprocessing";
+import { FloatingGrid } from "./FloatingGrid";
+
+function CarShow() {
+  return (
+    <>
+      <OrbitControls target={[0, 0.35, 0]} maxPolarAngle={1.45} />
+      <PerspectiveCamera makeDefault fov={50} position={[3, 2, 5]} />
+
+      <color args={[0, 0, 0]} attach="background" />
+
+      <CubeCamera resolution={256} frames={Infinity}>
+        {(texture) => (
+          <>
+            <Environment map={texture} />
+            <Car />
+          </>
+        )}
+      </CubeCamera>
+
+      <Rings />
+      <Boxes />
+      <FloatingGrid />
+
+      <spotLight
+        color={[1, 0.25, 0.7]}
+        intensity={1.5}
+        angle={0.6}
+        penumbra={0.5}
+        position={[5, 5, 0]}
+        castShadow
+        shadow-bias={-0.0001}
+      />
+
+      <spotLight
+        color={[0.14, 0.5, 1]}
+        intensity={2}
+        angle={0.6}
+        penumbra={0.5}
+        position={[-5, 5, 0]}
+        castShadow
+        shadow-bias={-0.0001}
+      />
+
+      <Ground />
+      <EffectComposer>
+        <DepthOfField
+          focusDistance={0.0035}
+          focalLength={0.01}
+          bokehScale={3}
+          height={480}
+        />
+        <Bloom
+          blendFunction={BlendFunction.ADD}
+          intensity={1.3}
+          width={300}
+          height={300}
+          kernelSize={5}
+          luminanceThreshold={0.15}
+          luminanceSmoothing={0.025}
+        />
+        <ChromaticAberration
+          blendFunction={BlendFunction.NORMAL}
+          offset={[0.0005, 0.0012]}
+        />
+      </EffectComposer>
+    </>
+  );
+}
+
+const App = () => (
+  <Suspense fallback={null}>
+    <Canvas shadows>
+      <CarShow />
+    </Canvas>
+  </Suspense>
+);
+
+export default App;
